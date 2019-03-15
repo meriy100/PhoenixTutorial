d4e836a19b001ea05544bb61d0418fcd2e52bf24
meriy100 <ttattataa@gmail.com>
2019-03-15 21:14:11 +0900

    add create

--- lib/project_name_web/controllers/posts_controller.ex
+++ lib/project_name_web/controllers/posts_controller.ex
@@ -2,11 +2,18 @@ defmodule ProjectNameWeb.PostsController do
   use ProjectNameWeb, :controller
 
   def index(conn, _params) do
-    post = %ProjectName.Post{title: "タイトル", views: 1}
-    IO.puts post.title
-    # ProjectName.Repo.insert!(post)
-    post = ProjectName.Repo.get ProjectName.Post, 1
+    post = ProjectName.Post.changeset(%ProjectName.Post{}, %{title: "タイトル", views: 1})
+#    IO.puts post.title
+#    post = ProjectName.Repo.get ProjectName.Post, 1
 
     render conn, "index.html", foo: "bar2", post: post
+  end
+
+  def create(conn, %{"post" => %{ "title" => title} }) do
+    post = %ProjectName.Post{title: title, views: 1}
+    IO.puts post.title
+    ProjectName.Repo.insert!(post)
+
+    redirect(conn, to: "/posts")
   end
 end
---
+++ priv/repo/migrations/20190315112519_create_posts.exs
@@ -0,0 +1,13 @@
+defmodule ProjectName.Repo.Migrations.CreatePosts do
+  use Ecto.Migration
+
+  def change do
+    create table(:posts) do
+      add :title, :string
+      add :views, :integer
+
+      timestamps()
+    end
+
+  end
+end
